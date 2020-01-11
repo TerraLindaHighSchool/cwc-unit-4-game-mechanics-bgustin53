0922eaeddcc3d6c3fccfb47ccc9fc47c3452fee8
Bruce Gustin <bgustin@srcs.org>
2020-01-11 09:17:40 -0800

    Completed 4.3.4

--- Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
 
     public float speed = 5.0f;
+    public bool hasPowerup = false;
     
     // Start is called before the first frame update
     void Start()
@@ -21,5 +22,22 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Collided with: " + collision.gameObject.name);
+        }
     }
 }
